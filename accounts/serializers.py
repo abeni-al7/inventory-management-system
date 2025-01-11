b71ac11724ea151a5f01c57c55dfe50d83e9b700
abeni-al7 <abenezeralebachew3@gmail.com>
2025-01-11 22:48:25 +0300

    Use custom serializer to add user info to auth endpoint

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,31 +5,34 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from rest_framework import serializers
 from django.core.mail import send_mail
 from django.conf import settings
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 User = get_user_model()
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'username', 'password')
+        fields = ("email", "first_name", "last_name", "phone", "username", "password")
         extra_kwargs = {
-            'password': {'write_only': True},
+            "password": {"write_only": True},
         }
 
     def get_fields(self):
         fields = super().get_fields()
-        request = self.context.get('request', None)
-        if request and request.method != 'POST':
-            fields.pop('password', None)
+        request = self.context.get("request", None)
+        if request and request.method != "POST":
+            fields.pop("password", None)
         return fields
-    
+
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
+        password = validated_data.pop("password", None)
         user = super().create(validated_data)
         if password:
             user.set_password(password)
             user.save()
         return user
+
 
 class PasswordResetSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -40,8 +43,8 @@ class PasswordResetSerializer(serializers.Serializer):
         return value
 
     def save(self):
-        request = self.context.get('request')
-        user = User.objects.get(email=self.validated_data['email'])
+        request = self.context.get("request")
+        user = User.objects.get(email=self.validated_data["email"])
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
         reset_url = f"{request.scheme}://{request.get_host()}/accounts/password-reset-confirm/{uid}/{token}/"
@@ -52,19 +55,21 @@ class PasswordResetSerializer(serializers.Serializer):
             recipient_list=[user.email],
         )
 
+
 class SetNewPasswordSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
     password_confirm = serializers.CharField(write_only=True)
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password_confirm']:
+        if attrs["password"] != attrs["password_confirm"]:
             raise serializers.ValidationError("Passwords do not match.")
         return attrs
 
     def save(self, user):
-        user.set_password(self.validated_data['password'])
+        user.set_password(self.validated_data["password"])
         user.save()
         return user
+
 
 class PasswordChangeSerializer(serializers.Serializer):
     old_password = serializers.CharField(write_only=True)
@@ -72,17 +77,43 @@ class PasswordChangeSerializer(serializers.Serializer):
     new_password_confirm = serializers.CharField(write_only=True)
 
     def validate_old_password(self, value):
-        user = self.context['request'].user
+        user = self.context["request"].user
         if not user.check_password(value):
             raise serializers.ValidationError("Old password is not correct.")
         return value
 
     def validate(self, attrs):
-        if attrs['new_password'] != attrs['new_password_confirm']:
+        if attrs["new_password"] != attrs["new_password_confirm"]:
             raise serializers.ValidationError("New passwords do not match.")
         return attrs
 
     def save(self, user):
-        user.set_password(self.validated_data['new_password'])
+        user.set_password(self.validated_data["new_password"])
         user.save()
         return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token["email"] = user.email
+        token["username"] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data.update(
+            {
+                "user": {
+                    "id": self.user.id,
+                    "email": self.user.email,
+                    "username": self.user.username,
+                    "first_name": self.user.first_name,
+                    "last_name": self.user.last_name,
+                    "phone": self.user.phone,
+                }
+            }
+        )
+        return data
--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from accounts.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
     path("", include("inventory.urls")),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
